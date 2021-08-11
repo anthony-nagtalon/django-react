71dff22326ac7882e02952593cad90482bb603b4
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-08-11 15:49:43 -0700

    Demo for permissions and tests for them

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(max_length=10, choices=options, default='published')
+    objects = models.Manager()
     postobjects = PostObjects()
 
     class Meta:
